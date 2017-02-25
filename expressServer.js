46768fc487398978626f01a3b64f567c53909521
Ubuntu <HAHAHARVEY@HAHAHARVEY.gkqveos0zooulkjzl0hccypcaf.lx.internal.cloudapp.net>
2017-02-25 13:10:01 +0000

    Add frontend

--- expressServer.js
+++ expressServer.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
+app.use('/', express.static(path.join(__dirname, 'static')));
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(3000, function () {
